611841ebfa9b7049527c0e101a8e1199c8dddbfd
Eammon365 <42590346+Eammon365@users.noreply.github.com>
2020-02-04 22:20:04 +0000

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -19,35 +19,36 @@ namespace GradeBook.GradeBooks
                 throw new InvalidOperationException("Ranked Grading requires more than 5 grades to caclulate an average ");
             }
 
-                //Will fe
-                var theshold = (int)Math.Ceiling(Students.Count * 0.2);
+            //Will fe
+            var theshold = (int)Math.Ceiling(Students.Count * 0.2);
 
-                var grades = Students.OrderByDescending(e => e.AverageGrade)
-                    .Select(e => e.AverageGrade)
-                    .ToList();
+            var grades = Students.OrderByDescending(e => e.AverageGrade)
+                .Select(e => e.AverageGrade)
+                .ToList();
 
-                if (grades[theshold - 1] <= averageGrade)
-                {
-                    return 'A';
-                }
-                else if (grades[theshold * 2 - 1] <= averageGrade)
-                {
-                    return 'B';
-                }
-                else if (grades[theshold * 3 - 1] <= averageGrade)
-                {
-                    return 'C';
-                }
-                else if (grades[theshold * 4 - 1] <= averageGrade)
-                {
-                    return 'D';
-                }
-                else
-                {
-                    return 'F';
-                }
-            
-                
-            
+            if (grades[theshold - 1] <= averageGrade)
+            {
+                return 'A';
+            }
+            else if (grades[theshold * 2 - 1] <= averageGrade)
+            {
+                return 'B';
+            }
+            else if (grades[theshold * 3 - 1] <= averageGrade)
+            {
+                return 'C';
+            }
+            else if (grades[theshold * 4 - 1] <= averageGrade)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+
+
+
         }
     }
+}
